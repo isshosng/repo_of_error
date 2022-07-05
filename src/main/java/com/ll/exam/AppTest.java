cbed904acfe8e687d264825ecac9c3722ffb1b05
isshosng <purehug@hanmail.net>
2022-07-05 13:24:18 +0900

    error

---
+++ src/main/java/com/ll/exam/AppTest.java
@@ -0,0 +1,13 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertations.assertEquals;
+
+public class AppTest {
+    @Test
+    public void 더하기() {
+        assertEquals();
+        assertTrue();
+
+    }
+}
